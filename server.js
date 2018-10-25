26a09bab7f875308e57ee6011d1b97a628b72b21
ntsmitty <nicktsmith7@gmail.com>
2018-10-25 14:46:35 -0700

    added testing route, to ensure client can make requets from server

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const app = express(); 
+const express = require('express');; 
+const path = require('path')
+const app = express()
 const PORT = 5000
 
 
-app.get('/testing', (req, res) => res.send('hello'))
+app.use( (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', "Origin X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
-
+app.get('/testing', (req, res) => {
+  res.status(200);
+  res.json([ 
+    {id: 1, customer: "Dumb Data"},
+    {id: 2, customer: "Stupid Data"},
+  ]);
+}); 
 
 
 app.listen(PORT, () => console.log('listening on 5000')); 
--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = { users: [] }
+
+  componentDidMount() { 
+   return fetch("http://localhost:5000/testing", {
+  
+   })
+     .then(res => res.json())
+     .then(users => this.setState({ users }))
+     .catch((err) => console.log('Error Caught')) 
+  }; 
+
   render() {
+    console.log(this.state.users)
    return (
-     <div>hi</div>
-   )
+    <div>{this.state.users}</div>
+    )
   }
 }
 
