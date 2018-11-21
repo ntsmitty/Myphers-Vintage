4a2ed2134743990484f5d4cd72d36dff4ad0e703
ntsmitty <nicktsmith7@gmail.com>
2018-11-20 17:14:01 -0800

    added productcard flip animation

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');; 
-const path = require('path')
-const app = express()
-const PORT = 5000
+// const express = require('express');; 
+// const path = require('path')
+// const app = express()
+// const PORT = 5000
 
 
-app.use( (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Headers', "Origin X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use( (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header('Access-Control-Allow-Headers', "Origin X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
-app.get('/testing', (req, res) => {
-  res.status(200);
-  res.set("Content-Type", "application/json")
-  res.json([ 
-    {id: 1, customer: "Dumb Data"},
-    {id: 2, customer: "Stupid Data"},
-  ]);
-}); 
+// app.get('/testing', (req, res) => {
+//   res.status(200);
+//   res.set("Content-Type", "application/json")
+//   res.json([ 
+//     {id: 1, customer: "Dumb Data"},
+//     {id: 2, customer: "Stupid Data"},
+//   ]);
+// }); 
 
 
-app.listen(PORT, () => console.log('listening on 5000')); 
+// app.listen(PORT, () => console.log('listening on 5000')); 
